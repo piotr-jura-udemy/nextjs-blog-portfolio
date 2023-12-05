787a58f12066d2942a819c25f039e5b17327ce18
Piotr Jura <jura.piotrek@gmail.com>
2023-12-05 19:30:08 +0100

    032-components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+function Card(props) {
+  return (
+    <div className="border rounded-md border-gray-600 p-4">
+      Card component: {props.text}
+    </div>
+  )
+}
+
 export default function Home() {
   const name = 'Piotr'
   return (
     <>
-      <div className="bg-gray-700">
-        Hello, {name}
+      <div className="p-20 space-y-4">
+        <div>Hello, {name}</div>
+        <Card text="This is being passed from the parent" />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   )
