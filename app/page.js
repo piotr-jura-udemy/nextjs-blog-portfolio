031781b7d1e9302c0fd3c890fbed106500820b98
Piotr Jura <jura.piotrek@gmail.com>
2023-12-07 11:14:49 +0100

    037-list-of-elements

--- app/page.js
+++ app/page.js
@@ -12,26 +12,27 @@ function Card({ children }) {
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(true)
+  const [names, setNames] = useState(['Piotr', 'John', 'Terry'])
   const name = 'Piotr'
   const handleClick = () => {
     setIsVisible(!isVisible)
   }
-  const cards = isVisible && <><Card>This is being passed from the parent</Card>
-    <Card>
-      <div>This is JS!</div>
-      <Card>Nested text!</Card>
-    </Card>
-    <Card />
-    <Card />
-    <Card /></>
+  const handleAdd = () => {
+    setNames([...names, 'New element!'])
+  }
+  const cards = isVisible
+    && names.map((name, index) => <Card key={index}>{name}</Card>)
   return (
     <>
       <div className="p-20 space-y-4">
         <div>Hello, {name}</div>
         {cards}
-        <button onClick={handleClick}>
-          {isVisible ? 'Hide' : 'Show'}
-        </button>
+        <div className="flex space-x-4">
+          <button onClick={handleClick}>
+            {isVisible ? 'Hide' : 'Show'}
+          </button>
+          <button onClick={handleAdd}>Add</button>
+        </div>
       </div>
     </>
   )
