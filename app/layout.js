71272394eac04e7db96ca7f55137179816aac8e0
Piotr Jura <jura.piotrek@gmail.com>
2023-12-08 14:58:08 +0100

    051-layouts

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="p-20 border border-yellow-400">
+          Iam be visible everywhere!
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
