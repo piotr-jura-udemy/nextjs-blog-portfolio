01022b81e5cba2e8402339d5deea63d58d0ee4b3
Piotr Jura <jura.piotrek@gmail.com>
2023-12-13 12:14:14 +0100

    057-fetching-data-using-fetch

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,3 +1,20 @@
-export default function ProjectsPage() {
-  return (<>Projects!</>)
+export default async function ProjectsPage() {
+  const response = await fetch('http://localhost:3001/repos')
+  const repos = await response.json()
+
+  return (
+    <div className="p-20">
+      <h1 className="mb-8 text-xl">Projects</h1>
+
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.id} className="mb-4">
+            <div>{repo.title}</div>
+            <div>{repo.description}</div>
+            <div>{repo.stargazers_count}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
