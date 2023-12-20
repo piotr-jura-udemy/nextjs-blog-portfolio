ad21d2fcfaceafb6cc410b28a5c9288b80f57a9c
Piotr Jura <jura.piotrek@gmail.com>
2023-12-20 19:02:19 +0100

    069-positioning

--- components/header.js
+++ components/header.js
@@ -1,9 +1,18 @@
 import Navigation from "./navigation"
+import Link from "next/link"
 
 export default function Header() {
   return (
-    <header>
-      <Navigation />
+    <header className="flex justify-between md:items-center mt-4">
+      <div className="flex items-center md:space-x-12">
+        <div className="hidden md:block">
+          <Link href="/" className="text-xl">Piotr Jura</Link>
+        </div>
+        <Navigation />
+      </div>
+      <div>
+        Dark Toggle
+      </div>
     </header>
   )
 }
