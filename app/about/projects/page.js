440634832bc60a64d86765570599d36d51698f08
Piotr Jura <jura.piotrek@gmail.com>
2023-12-27 16:08:32 +0100

    081-react-error-boundary

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,15 +1,18 @@
 import { Suspense } from "react";
 import ProjectList from "./components/project-list";
 import ProjectListLoading from "./components/project-list-loading";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function ProjectsPage() {
   return (
     <div>
       <h1 className="mb-8 text-xl">Projects</h1>
       <div className="mb-8">Hello, this is the list of my repos!</div>
-      <Suspense fallback={<ProjectListLoading />}>
-        <ProjectList />
-      </Suspense>
+      <ErrorBoundary fallback={<div>Cannnot fetch projects currently</div>}>
+        <Suspense fallback={<ProjectListLoading />}>
+          <ProjectList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
