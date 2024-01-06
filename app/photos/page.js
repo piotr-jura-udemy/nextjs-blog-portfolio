07a9239d503c1cd939ebb98a73dd97b382ee9376
Piotr Jura <jura.piotrek@gmail.com>
2024-01-06 15:06:12 +0100

    105-quality-placeholder-priority

--- app/photos/page.js
+++ app/photos/page.js
@@ -14,16 +14,16 @@ export default function PhotosPage() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="h-60 overflow-hidden relative">
-          <Image fill src={dog1} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
+          <Image fill src={dog1} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" priority={true} quality={50} placeholder="blur" />
         </div>
         <div className="relative h-60 overflow-hidden">
-          <Image fill src={dog2} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
+          <Image fill src={dog2} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" priority={true} quality={50} placeholder="blur" />
         </div>
         <div className="relative h-60 overflow-hidden">
-          <Image fill src={dog3} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
+          <Image fill src={dog3} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" quality={50} placeholder="blur" />
         </div>
         <div className="relative h-60 overflow-hidden">
-          <Image fill src={dog4} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
+          <Image fill src={dog4} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" quality={50} placeholder="blur" />
         </div>
       </div>
     </div>
