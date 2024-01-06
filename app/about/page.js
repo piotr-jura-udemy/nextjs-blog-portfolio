ef1b90a2c4a2441e1164f6c0aa29eb05e8428d37
Piotr Jura <jura.piotrek@gmail.com>
2024-01-06 15:05:45 +0100

    104-image-sizes

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,3 @@
 export default function AboutPage() {
-  if (Math.random() > 0.5) throw new Error('ooops')
   return (<>About me!</>)
 }
--- app/photos/page.js
+++ app/photos/page.js
@@ -12,18 +12,18 @@ export default function PhotosPage() {
         My Photos
       </h1>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="h-60 overflow-hidden relative">
-          <Image fill src={dog1} className="object-cover w-full h-full" alt="Picture of my dog" />
+          <Image fill src={dog1} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
         </div>
         <div className="relative h-60 overflow-hidden">
-          <Image fill src={dog2} className="object-cover w-full h-full" alt="Picture of my dog" />
+          <Image fill src={dog2} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
         </div>
-        <div className="h-60 overflow-hidden">
-          <Image src={dog3} className="object-cover w-full h-full" alt="Picture of my dog" />
+        <div className="relative h-60 overflow-hidden">
+          <Image fill src={dog3} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
         </div>
-        <div className="h-60 overflow-hidden">
-          <Image src={dog4} className="object-cover w-full h-full" alt="Picture of my dog" />
+        <div className="relative h-60 overflow-hidden">
+          <Image fill src={dog4} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" />
         </div>
       </div>
     </div>
