b2a2dc75c20daeb03aefab66ca56d81e36657e03
Piotr Jura <jura.piotrek@gmail.com>
2024-01-06 18:08:49 +0100

    110-fonts

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         <main className="mt-12">
           {children}
