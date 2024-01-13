a1a149fe755f851b0f3996173f0f3e68f10f98cd
Piotr Jura <jura.piotrek@gmail.com>
2024-01-13 21:20:38 +0100

    137-blog-post-list-displaying

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { getPost } from '@/lib/posts'
 import fs from 'fs'
 import path from 'path'
@@ -24,7 +25,20 @@ export default async function BlogPostsPage() {
 
   return (
     <>
-      <h1>Recent Posts</h1>
+      <h1 className="mb-8 text-xl">Recent Posts</h1>
+
+      <div className="text-lg text-gray-600 dark:text-gray-400 mb-8">Stay up to date with most recent posts</div>
+
+      <hr />
+
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`} className="text-2xl font-semibold text-gray-800 dark:text-gray-200">{post.frontmatter.title}</Link>
+            <div className="text-gray-400 text-sm mt-2">{post.frontmatter.date}</div>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
--- components/navigation.js
+++ components/navigation.js
@@ -6,9 +6,6 @@ export default function Navigation() {
     <nav className="font-mono">
       <ul className="flex md:space-x-4 flex-col md:flex-row">
         <li>
-          <Link href="/" className={styles.link}>Home</Link>
-        </li>
-        <li>
           <Link href="/about" className={styles.link}>About</Link>
         </li>
         <li>
@@ -16,6 +13,9 @@ export default function Navigation() {
         </li>
         <li>
           <Link href="/photos" className={styles.link}>Photos</Link>
+        </li>
+        <li>
+          <Link href="/blog" className={styles.link}>Blog</Link>
         </li>
       </ul>
     </nav>
